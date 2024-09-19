9075efb4aa41ce2989d2ae8899c92dced8ff9470
Reinel-Quintero <rquinter8@cuc.edu.co>
2024-09-19 11:42:11 -0500

    m1

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+import miLogo from "./assets/img/cheems.png"
+
+
+function App() {
+
+  return (
+    <>
+    <div>
+      <img src={miLogo} alt="cheems" />
+      <h1>Hola Profe</h1>
+      <p> este es mi primer proyecto en react</p>
+    </div>
+    </>
+  )
+}
+
+export default App
